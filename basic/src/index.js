1e894d8af15c2a90bda6275d0a58215ae0b7ab2b
shreyapalat2023 <garimapalat@gmail.com>
2024-06-12 11:37:31 +0530

    basic

---
+++ basic/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HomePage from "./homePage.jsx";
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+// import { createRoot } from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// function Mycompo (){
+//   return "data";
+// }
+// we cannot define function name like this.First letter should be capital.
+
+// function mycompo (){
+//   return "data";
+// }
+
+root.render(<HomePage/>);
+
+
+
+
+
